83e5ba55725c47c47f3ad68a02913ff79c2234b9
Oliver Martínez <martinezharooliver@gmail.com>
2026-03-06 13:47:48 +0100

    refactor: move static data to separate module

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const people = [
+	{
+		name: "Gracie Abrams",
+		username: "gracieabrams",
+		img: "https://images.ecestaticos.com/FHMwU_9LbV3iEeo2zFVAo8tGtcw=/47x0:2215x1626/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F091%2Fd78%2F8f2%2F091d788f2f1e367b6a0eee6b48580288.jpg",
+		isFollowing: false,
+	},
+	{
+		name: "Oliver Martínez",
+		username: "martinezharo",
+		img: "https://olivermartinezharo.com/icon.svg",
+		isFollowing: false,
+	},
+	{
+		name: "Fernando Alonso",
+		username: "alo14",
+		img: "https://cdn-7.motorsport.com/images/mgl/0ZRQbbN0/s800/fernando-alonso-aston-martin-r.jpg",
+		isFollowing: true,
+	},
+	{
+		name: "Marcus Aurelius",
+		username: "maurelius",
+		img: "https://img.freepik.com/fotos-premium/retrato-estoico-marco-aurelio-pelicula-blanco-negro-grano-obra-maestra-muy-detallada_1097265-29435.jpg?w=360",
+		isFollowing: false,
+	},
+	{
+		name: "Olivia Rodrigo",
+		username: "olivia_rdrg",
+		img: "https://hips.hearstapps.com/hmg-prod/images/olivia-rodrigo-deja-vu-1617092938.jpg?crop=1.00xw:0.913xh;0,0.0102xh&resize=1200:*",
+		isFollowing: true,
+	},
+];
+
+export default people;
